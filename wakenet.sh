c3c6643f392f93c9af0c93603d4cb27f58546193
Dolphykins <roflawl2009@gmail.com>
2016-08-20 16:09:55 -0600

    Update wakenet.sh

--- wakenet.sh
+++ wakenet.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+## This script will kill all network services and restart them.
+## Useful for my laptop which tends to fail to re-initialize the Network Adapter after suspend.
+## Credit: User "mrm" on Ubuntu Forums
+## Source: http://askubuntu.com/questions/368671/ubuntu-13-10-wifi-not-re-connecting-after-suspend
 service network-manager restart
 case "$1" in
 thaw|resume)
